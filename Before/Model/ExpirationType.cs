08c522557d0994082009b02dfdf638fdb6c30cb6
Leshan Bashitha <leshanb@alliontechnologies.com>
2019-12-19 21:55:59 +0530

    Initial Commit

---
+++ Before/Model/ExpirationType.cs
@@ -0,0 +1,10 @@
+namespace Before.Model
+{
+    using System;
+
+    public enum ExpirationType
+    {
+        Assignment = 1,
+        Fixed = 2
+    }
+}
---
+++ Before/Model/Member.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace Before.Model
+{
+    using System;
+    using Services;
+
+    public class Member : Entity
+	{
+	    public Member()
+		{
+            AssignedOffers = new List<Offer>();
+		}
+
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public ICollection<Offer> AssignedOffers { get; set; }
+        public int NumberOfActiveOffers { get; set; }
+
+    }
+}
